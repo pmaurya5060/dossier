4e66306def155e58759cb1b0ddd7d1ba03464e1e
pmaurya5060 <pmaurya5060@gmail.com>
2026-04-22 15:08:16 +0530

    imported tasklist and taskitems

--- src/app/documents/[documentid]/editor.tsx
+++ src/app/documents/[documentid]/editor.tsx
@@ -1,6 +1,7 @@
 "use client";
 
 import {useEditor,EditorContent} from "@tiptap/react";
+import { TaskItem,TaskList } from "@tiptap/extension-list";
 import StarterKit from "@tiptap/starter-kit";
 
 export const Editor=()=>{
@@ -13,7 +14,13 @@ export const Editor=()=>{
           "focus:outline-none print:border-0 bg-white border border-[#C7C7C7] flex flex-col min-h-[1054px] w-[816px] pt-10 pr-14 pl-14 pb-10 cursor-text",
       },
     },
-    extensions:[StarterKit],
+    extensions:[
+      StarterKit,
+      TaskItem.configure({
+        nested:true
+      }),
+      TaskList,
+    ],
     content:"<p>Hello World! 🌍</p>",
   });
   
