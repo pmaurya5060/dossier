7e6ffb7e859ccf0f4d8c224e528afcc05ed8761e
pmaurya5060 <pmaurya5060@gmail.com>
2026-04-22 15:00:29 +0530

    Style given to the list items

--- src/app/documents/[documentid]/editor.tsx
+++ src/app/documents/[documentid]/editor.tsx
@@ -10,7 +10,7 @@ export const Editor=()=>{
       attributes:{
         styles:"padding-left:56px;padding-right:56px",
         class:
-          "focus:outline-none print:border-0 bg-white border border-[#C7C7C7] flex flex-col min-h-[1054px] w-[816px] pt-10 pr-14 pb-10 cursor-text",
+          "focus:outline-none print:border-0 bg-white border border-[#C7C7C7] flex flex-col min-h-[1054px] w-[816px] pt-10 pr-14 pl-14 pb-10 cursor-text",
       },
     },
     extensions:[StarterKit],
