f671cdbd5aa2441580f7293a543825d3afd3d9f0
pmaurya5060 <pmaurya5060@gmail.com>
2026-06-24 02:58:29 +0530

    added headings dropdown

--- src/app/documents/[documentid]/toolbar.tsx
+++ src/app/documents/[documentid]/toolbar.tsx
@@ -6,6 +6,66 @@ import {useEditorStore} from '@/store/use-editor-store';
 import { Separator } from "../../../components/ui/separator"
 import { DropdownMenu,DropdownMenuContent,DropdownMenuItem,DropdownMenuTrigger } from "@/src/components/ui/dropdown-menu";
 import { Button } from "@/src/components/ui/button";
+import {type Level} from "@tiptap/extension-heading";
+
+const HeadingLevelButton=()=>{
+    const {editor}= useEditorStore();
+
+    const Headings=[
+        {label:"Normal Text" , value:0, fontSize:"16px"},
+        {label:"Heading 1", value: 1, fontSize: "32px"}, 
+        {label:"Heading 2", value: 2, fontSize: "24px"}, 
+        {label:"Heading 3", value: 3, fontSize: "20px"}, 
+        {label:"Heading 4", value: 4, fontSize: "18px"}, 
+        {label:"Heading 5", value: 5, fontSize: "16px"}, 
+    ]
+
+    const getCurrentHeading=()=>{
+        for(let level=1;level<=5;level++){
+            if(editor?.isActive("heading",{level})){
+                return `Heading ${level}`;
+            }
+        }
+        return "Normal Text";
+    }
+    return(
+        <DropdownMenu>
+        <DropdownMenuTrigger asChild>
+            <button className="h-7 min-w-7 shrink-0 flex items-center justify-center rounded-sm hoveer:bg-neutral-200/80 px-1.5 overflow-hidden text-sm">
+                <span className="truncate">
+                    {getCurrentHeading()}
+                </span>
+                <ChevronDownIcon className="ml-2 size-4 shrink-0"></ChevronDownIcon>
+            </button>
+
+        </DropdownMenuTrigger>
+        <DropdownMenuContent className="p-1 flex flex-col gap-y-1">
+            {Headings.map(({label,value,fontSize})=>(
+
+                <button
+                onClick={()=>{
+                    if(value==0){
+                        editor?.chain().focus().setParagraph().run();
+                    }else{
+                        editor?.chain().focus().toggleHeading({level:value as Level}).run();
+                    }
+                }}
+                key={value}
+                style={{fontSize}}
+                className={cn(
+                    "flex items-center gap-x-2 px-2 py-1 rounded-sm hover:bg-neutral-200/80",
+                    (value==0 && !editor?.isActive("heading")) || editor?.isActive("heading", {level:value}) && "bg-neutral-200/80"
+                )}                
+                >
+                    {label}
+                </button>
+            ))}
+
+        </DropdownMenuContent>
+        </DropdownMenu>
+
+    )
+}
 
 const FontFamilyButton = () => {
   const { editor } = useEditorStore();
@@ -21,6 +81,12 @@ const FontFamilyButton = () => {
   return(
     <DropdownMenu>
         <DropdownMenuTrigger asChild>
+            {/* </DropdownMenuTrigger>Radix UI: Use `asChild` when providing your own element. */}
+            {/* </DropdownMenu> </DropdownMenuTrigger>Without `asChild`, Radix renders its own element (e.g. <button>). */}
+            {/* </DropdownMenuTrigger>Wrapping a <button> inside DropdownMenuTrigger creates: */}
+            {/* </DropdownMenuTrigger><button><button>...</button></button> */}
+            {/* </DropdownMenuTrigger>causing invalid HTML and hydration errors. */}
+            {/* </DropdownMenuTrigger>`asChild` tells Radix to use the child element instead of creating a new one. */}
             <button className="h-7 w-30 shrink-0 flex items-center justify-between rounded-sm hoveer:bg-neutral-200/80 px-1.5 overflow-hidden text-sm">
                 <span className="truncate">
                     {editor?.getAttributes("textStyle").fontFamily || "Ariel"}
@@ -32,6 +98,7 @@ const FontFamilyButton = () => {
         <DropdownMenuContent className="p-1 flex flex-col gap-y-1">
             {fonts.map(({label,value})=>(
                 <button 
+                onClick={()=>editor?.chain().focus().setFontFamily(value).run()}
                 key={value}
                 className={cn(
                     "flex items-center gap-x-2 px-2 py-1 rounded-sm hover:bg-neutral-200/80",
@@ -246,7 +313,9 @@ export const Toolbar=()=>{
                 <FontFamilyButton/>
             </Separator>
 
-            <Separator orientation="vertical" className="h-6 bg-neutral-200"></Separator>
+            <Separator orientation="vertical" className="h-6 bg-neutral-200">
+                <HeadingLevelButton/>
+            </Separator>
 
             <Separator orientation="vertical" className="h-6 bg-neutral-200"></Separator>
 
